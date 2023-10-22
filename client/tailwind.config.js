6938f631aced7432bc1f57fab121994b3aa80bb4
Mell Lessington <mjlessington@gmail.com>
2023-10-22 14:47:41 -0400

    Lando integration and flowbite/tailwind correction

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,11 @@
-import flowbitePlugin from 'flowbite/plugin';
-
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    flowbitePlugin
-  ],
-}
-
-
+export const content = [
+  './src/**/*.{js,jsx,ts,tsx}',
+  'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [
+  'flowbite/plugin'
+];
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { 
+    https: false,
+    host: true,
+    port: 5173,
+    hmr: {host: 'localhost', protocol: 'ws'},
+},
 })
