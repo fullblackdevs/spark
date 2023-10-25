dac3a3bdf933a1d490a3c0c781dadc1de6742960
Miquel Brazil <miquel@brazilliance.co>
2023-10-25 17:37:19 -0400

    task: Add proxy url settings and fix HMR error
    Add Lando configuration settings to allow UI to be accessed by `spark-ui.local` as well as instructions for making it available in the local dev environment.
    
    Changed the Lando project name to distinguish UI and API naming.
    
    Add a `port` setting to the `vite.hmr` configuration to fix a websocket error in the browser console.

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { 
+  server: {
     https: false,
     host: true,
     port: 5173,
-    hmr: {host: 'localhost', protocol: 'ws'},
+    hmr: {host: 'localhost', port: 5173, protocol: 'ws'},
 },
 })
