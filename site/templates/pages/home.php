f1c307c2296173953a9a1aec22bb1da37eb7a25b
Miquel Brazil <miquel@brazilliance.co>
2023-12-29 16:29:56 -0500

    task: End of session commit

--- site/templates/pages/home.php
+++ site/templates/pages/home.php
@@ -1,14 +1,59 @@
-<div class="bg-[#FAF3F9] grow min-h-full *:px-28 text-black *:py-16">
-	<section class="flex min-h-[676px] bg-courageous-plum-100 relative items-center overflow-hidden">
-		News & Events
+<div class="bg-[#FAF3F9] grow min-h-full *:px-28 text-black">
+	<section class="flex flex-col bg-courageous-plum-100 relative gap-8 py-24">
+		<h2 class="text-8xl font-display uppercase tracking-tight text-courageous-plum">The Latest</h2>
+		<div class="flex gap-8">
+			<div class="flex flex-col w-2/3 border border-gray-200 rounded-lg shadow overflow-hidden bg-[url('/assets/images/ian-schneider-PAykYb-8Er8-unsplash.jpg')] bg-cover">
+				<div class="flex flex-col justify-between grow h-64 p-4 bg-gradient-to-t from-courageous-plum-900">
+					<div class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400 w-fit">Article</div>
+					<div class="flex items-start p-4 gap-4">
+						<div class="relative grow">
+							<h4 class="text-2xl tracking-tighter font-semibold text-courageous-plum-100">Spark Rings in the New Year with a New Look</h4>
+							<p class="font-medium tracking-tighter text-courageous-plum-200">Sandyanne Munroe</p>
+							<p class="text-xs mt-1 text-courageous-plum-200">Friday, December 29, 2023</p>
+							<a href="/event/we-the-people" class="absolute bottom-0 right-0 w-fit inline-flex items-center px-4 py-2 text-xs font-medium text-center text-courageous-plum border-2 border-courageous-plum rounded-lg hover:bg-courageous-plum-300 hover:text-white focus:ring-4 focus:outline-none focus:ring-courageous-plum-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+								Read more
+								<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+								</svg>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="flex flex-col w-1/3 gap-4">
+				<div class="flex flex-col grow border border-gray-200 rounded-lg shadow overflow-hidden bg-[url('/assets/images/thought-catalog-23KdVfc395A-unsplash.jpg')] h-64 bg-cover">
+					<div class="flex flex-col grow justify-between p-4 bg-gradient-to-t from-courageous-plum-600">
+						<div class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400 w-fit">Upcoming Event</div>
+						<div class="relative">
+							<h4 class="text-lg font-semibold text-courageous-plum-100 tracking-tighter">Women's Vision Roundtable</h4>
+							<time class="font-medium text-courageous-plum-200 tracking-tighter">Friday, December 1, 2023</time>
+							<p class="text-xs mt-1 text-courageous-plum-200"><span class="pe-4">Miami, FL</p>
+						</div>
+					</div>
+				</div>
+				<div class="flex flex-col grow border border-gray-200 rounded-lg shadow overflow-hidden bg-[url('/assets/images/imattsmart-v-uoqL494Gs-unsplash.jpg')] bg-cover">
+					<div class="flex flex-col justify-between h-64 p-4 bg-gradient-to-t from-courageous-plum-600">
+						<div class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400 w-fit">Resource</div>
+						<div class="relative">
+							<h4 class="text-lg font-semibold text-courageous-plum-100">Winter Food Pantry</h4>
+							<time class="font-medium tracking-tighter text-courageous-plum-200">AIDS Healthcare Foundation</time>
+							<p class="text-xs mt-1 text-courageous-plum-200">Ft. Lauderdale, FL</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</section>
-	<section class="flex min-h-[676px] bg-spark-pink relative items-center overflow-hidden">
+	<section class="flex min-h-[676px] bg-spark-pink relative items-center overflow-hidden py-16">
 		<svg viewBox="0 0 250 115" class="font-display w-96 text-7xl tracking-tighter uppercase">
 			<text y="55" class="fill-none stroke-1 stroke-courageous-plum tracking-tight" textLength="100%" lengthAdjust="spacingAndGlyphs">Partner</text>
 			<text y="112" textLength="100%" lengthAdjust="spacingAndGlyphs" class="fill-courageous-plum">Spotlight</text>
 		</svg>
-		<div class="flex grow bg-white rounded-xl overflow-hidden min-w-96 h-96 place-self-center justify-items-center relative z-40 ml-28">
-			<img src="/assets/images/otc-ahf.png" alt="" class="w-full h-full object-cover" />
+		<div class="flex grow shrink-0 bg-slate-800 rounded-xl overflow-hidden h-96 w-96 aspect-square place-self-center justify-stretch items-stretch relative z-40 ml-28 transition-all duration-300 ease-in-out bg-[url('/assets/images/otc-ahf.png')] bg-center bg-scalable hover:bg-scale-10 bg-no-repeat">
+			<div class="flex flex-col justify-center items-center grow text-white">
+				<div class="text-2xl">Logo</div>
+				<div class="mt-10">Link</div>
+			</div>
 		</div>
 		<svg viewbox="0 0 976 976" class="absolute -top-1/2 right-0 w-[976px] -mr-[45%] z-20">
 			<defs>
--- site/templates/pages/resources.php
+++ site/templates/pages/resources.php
@@ -6,9 +6,9 @@
 			</a>
 			<div class="grow flex flex-col p-5">
 				<a href="#">
-					<h4 class="mb-2 text-2xl font-bold tracking-tight text-courageous-plum-900 dark:text-white">AHF Pharmacy</h4>
+					<h4 class="mb-2 text-2xl font-bold tracking-tight text-courageous-plum-900">AHF Pharmacy</h4>
 				</a>
-				<p class="grow mb-3 font-normal text-gray-700 dark:text-gray-400">AHF offers multiple services to complete an easy-to-use circle of care. With convenient pharmacy locations, healthcare centers, and specialty medicare programs, we've got you covered.</p>
+				<p class="grow mb-3 font-normal text-courageous-plum-700 text-sm leading-relaxed">AHF offers multiple services to complete an easy-to-use circle of care. With convenient pharmacy locations, healthcare centers, and specialty medicare programs, we've got you covered.</p>
 				<a href="/resource/ahf-pharmacy" class="w-fit inline-flex items-center px-3 py-2 text-sm font-medium text-center text-courageous-plum border-2 border-courageous-plum rounded-lg hover:bg-courageous-plum-300 hover:text-white focus:ring-4 focus:outline-none focus:ring-courageous-plum-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
 					Learn more
 					<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -19,16 +19,16 @@
 		</div>
 
 		<div class="flex flex-col max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-		<a href="#" class="h-[150px] overflow-hidden">
+			<a href="#" class="h-[150px] overflow-hidden">
 				<img class="rounded-t-lg" src="/assets/images/image-1.jpg" alt="" />
 			</a>
-			<div class="p-5">
+			<div class="grow flex flex-col p-5">
 				<a href="#">
-					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">AHF Healthcare Center</h5>
+					<h5 class="mb-2 text-2xl font-bold tracking-tight text-courageous-plum-900">AHF Healthcare Center</h5>
 				</a>
-				<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Visit an AHF Healthcare Center for expert care delivered by experienced and compassionate staff. With over 1 million lives in care worldwide, we provide advanced medical services designed specifically for you.</p>
-				<a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-					Read more
+				<p class="mb-4 font-normal text-courageous-plum-700 text-sm leading-relaxed">Visit an AHF Healthcare Center for expert care delivered by experienced and compassionate staff. With over 1 million lives in care worldwide, we provide advanced medical services designed specifically for you.</p>
+				<a href="/resource/ahf-healthcare" class="w-fit inline-flex items-center px-3 py-2 text-sm font-medium text-center text-courageous-plum border-2 border-courageous-plum rounded-lg hover:bg-courageous-plum-300 hover:text-white focus:ring-4 focus:outline-none focus:ring-courageous-plum-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+					Learn more
 					<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
 						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
 					</svg>
@@ -37,16 +37,16 @@
 		</div>
 
 		<div class="flex flex-col max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-		<a href="#" class="h-[150px] overflow-hidden">
+			<a href="#" class="h-[150px] overflow-hidden">
 				<img class="rounded-t-lg" src="/assets/images/image-1.jpg" alt="" />
 			</a>
-			<div class="p-5">
+			<div class="grow flex flex-col p-5">
 				<a href="#">
-					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Out of the Cloest</h5>
+					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Out of the Closet</h5>
 				</a>
-				<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Decidedly the world's most fabulous thrift store. Known to regulars as “the best-kept secret in town,” 96 cents of every dollar goes to HIV/AIDS services provided by AIDS Healthcare Foundation. <br />Shop or donate today!</p>
-				<a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-					Read more
+				<p class="mb-3 font-normal text-courageous-plum-700 text-sm leading-relaxed">Decidedly the world's most fabulous thrift store. Known to regulars as “the best-kept secret in town,” 96 cents of every dollar goes to HIV/AIDS services provided by AIDS Healthcare Foundation. Shop or donate today!</p>
+				<a href="/resource/ahf-otc" class="w-fit inline-flex items-center px-3 py-2 text-sm font-medium text-center text-courageous-plum border-2 border-courageous-plum rounded-lg hover:bg-courageous-plum-300 hover:text-white focus:ring-4 focus:outline-none focus:ring-courageous-plum-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+					Learn more
 					<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
 						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
 					</svg>
@@ -55,16 +55,16 @@
 		</div>
 
 		<div class="flex flex-col max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-			<a href="#">
+			<a href="#" class="h-[150px] overflow-hidden">
 				<img class="rounded-t-lg" src="/assets/images/image-1.jpg" alt="" />
 			</a>
-			<div class="p-5">
+			<div class="grow flex flex-col p-5">
 				<a href="#">
-					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">AHF Pharmacy</h5>
+					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">SunServe Safe T Support Group</h5>
 				</a>
-				<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-				<a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-					Read more
+				<p class="mb-3 font-normal text-courageous-plum-700 text-sm leading-relaxed">Drop-in support group for those who do not conform to gender stereotypes specifically for Transgender, and Gender-queer persons.</p>
+				<a href="/resource/sun-serve-safet-support-group" class="w-fit inline-flex items-center px-3 py-2 text-sm font-medium text-center text-courageous-plum border-2 border-courageous-plum rounded-lg hover:bg-courageous-plum-300 hover:text-white focus:ring-4 focus:outline-none focus:ring-courageous-plum-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+					Learn more
 					<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
 						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
 					</svg>
