be6da9459fb570bba1b0252c8361c4112712cf88
Miquel Brazil <miquel@brazilliance.co>
2024-01-08 02:10:32 -0500

    task: Improve Events Slider component
    Improve Events Slider component.

--- site/templates/pages/events.php
+++ site/templates/pages/events.php
@@ -1,58 +1,62 @@
 <div class="bg-[#FAF3F9] grow min-h-full px-28 text-black py-16">
-	<div x-data="{ currentFeaturedEvent: 1, showFeaturedEvent: (featuredEventID) => $data.currentFeaturedEvent = featuredEventID }" class="flex flex-col overflow-x-hidden">
-		<div class="flex shrink-0">
-			<div x-data="{ thisFeaturedEvent: 1 }" class="flex tracking-tight gap-8 mb-4" x-show="currentFeaturedEvent == thisFeaturedEvent || false" x-transition:enter=" transition ease-out duration-300" x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in duration-300" x-transition:leave-start="translate-x-0" x-transition:leave-end="-translate-x-full">
-				<div class=" aspect-square h-64">
-					<img src="/assets/images/image-1.jpg" alt="" class="w-full h-full rounded-xl bg-gray-50 object-cover" />
-				</div>
-				<div class="flex flex-col">
-					<div class="flex mb-1">
-						<div class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">Featured Event</div>
-						<div class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">In Person</div>
+	<div x-data="{ currentFeaturedEvent: 1, showFeaturedEvent: (featuredEventID) => $data.currentFeaturedEvent = featuredEventID }" class="flex flex-col overflow-hidden">
+		<div class="whitespace-nowrap w-full transition ease-in-out duration-300 relative mb-4" :style="'transform: translateX(-' + (currentFeaturedEvent - 1) * 100.5 + '%)'">
+			<div x-data="{ thisFeaturedEvent: 1 }" class="inline-block tracking-tight w-full whitespace-normal">
+				<div class="flex gap-8">
+					<div class=" aspect-square h-64">
+						<img src="/assets/images/image-1.jpg" alt="" class="w-full h-full rounded-xl bg-gray-50 object-cover" />
 					</div>
-					<div class="flex flex-col grow">
-						<h4 class="font-semibold text-3xl text-courageous-plum">We the People National March</h4>
-						<time class="text-courageous-plum-400 font-medium tracking-tighter">Sunday, July 2, 2023</time>
-						<p class="mt-4 leading-relaxed">Close to 1,000 bills are being introduced across the country that undermine basic human rights. The We the People Coalition is fighting back. Join us for the We The People March in Fort Lauderdale, FL, on July 2nd to protect our democracy.</p>
-					</div>
-					<div class="flex text-sm text-courageous-plum items-center border-t pt-4 border-courageous-plum-200">
-						<div class="font-medium pe-4">AIDS Healthcare Foundation</div>
-						<div class="border-courageous-plum-300 border-s px-4">Southern Region</div>
-						<div class="border-courageous-plum-300 border-s px-4 grow">Ft. Lauderdale, FL</div>
-						<a href="/event/we-the-people" class="w-fit inline-flex items-center px-4 py-2 text-sm font-medium text-center text-courageous-plum border-2 border-courageous-plum rounded-lg hover:bg-courageous-plum-300 hover:text-white focus:ring-4 focus:outline-none focus:ring-courageous-plum-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-							More info
-							<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-							</svg>
-						</a>
+					<div class="flex flex-col">
+						<div class="flex mb-1">
+							<div class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">Featured Event</div>
+							<div class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">In Person</div>
+						</div>
+						<div class="flex flex-col grow">
+							<h4 class="font-semibold text-3xl text-courageous-plum">We the People National March</h4>
+							<time class="text-courageous-plum-400 font-medium tracking-tighter">Sunday, July 2, 2023</time>
+							<p class="mt-4 leading-relaxed">Close to 1,000 bills are being introduced across the country that undermine basic human rights. The We the People Coalition is fighting back. Join us for the We The People March in Fort Lauderdale, FL, on July 2nd to protect our democracy.</p>
+						</div>
+						<div class="flex text-sm text-courageous-plum items-center border-t pt-4 border-courageous-plum-200">
+							<div class="font-medium pe-4">AIDS Healthcare Foundation</div>
+							<div class="border-courageous-plum-300 border-s px-4">Southern Region</div>
+							<div class="border-courageous-plum-300 border-s px-4 grow">Ft. Lauderdale, FL</div>
+							<a href="/event/we-the-people" class="w-fit inline-flex items-center px-4 py-2 text-sm font-medium text-center text-courageous-plum border-2 border-courageous-plum rounded-lg hover:bg-courageous-plum-300 hover:text-white focus:ring-4 focus:outline-none focus:ring-courageous-plum-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+								More info
+								<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+								</svg>
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
 
-			<div x-data="{ thisFeaturedEvent: 2 }" class="flex tracking-tight gap-8 mb-4" x-show="currentFeaturedEvent == thisFeaturedEvent || false" x-transition:enter=" transition ease-out duration-300" x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in duration-300" x-transition:leave-start="translate-x-0" x-transition:leave-end="-translate-x-full">
-				<div class=" aspect-square h-64">
-					<img src="/assets/images/image-1.jpg" alt="" class="w-full h-full rounded-xl bg-gray-50 object-cover" />
-				</div>
-				<div class="flex flex-col">
-					<div class="flex mb-1">
-						<div class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">Featured Event</div>
-						<div class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">In Person</div>
+			<div x-data="{ thisFeaturedEvent: 2 }" class="inline-block tracking-tight w-full whitespace-normal">
+				<div class="flex gap-8">
+					<div class=" aspect-square h-64">
+						<img src="/assets/images/image-1.jpg" alt="" class="w-full h-full rounded-xl bg-gray-50 object-cover" />
 					</div>
 					<div class="flex flex-col grow">
-						<h4 class="font-semibold text-3xl text-courageous-plum">AIDS is a Civil Rights Issue</h4>
-						<time class="text-courageous-plum-400 font-medium tracking-tighter">Monday, January 15, 2024</time>
-						<p class="mt-4 leading-relaxed">This is a big event.</p>
-					</div>
-					<div class="flex text-sm text-courageous-plum items-center border-t pt-4 border-courageous-plum-200">
-						<div class="font-medium pe-4">AIDS Healthcare Foundation</div>
-						<div class="border-courageous-plum-300 border-s px-4">Southern Region</div>
-						<div class="border-courageous-plum-300 border-s px-4 grow">Atlanta, GA</div>
-						<a href="/event/we-the-people" class="w-fit inline-flex items-center px-4 py-2 text-sm font-medium text-center text-courageous-plum border-2 border-courageous-plum rounded-lg hover:bg-courageous-plum-300 hover:text-white focus:ring-4 focus:outline-none focus:ring-courageous-plum-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-							More info
-							<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-							</svg>
-						</a>
+						<div class="flex mb-1">
+							<div class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400">Featured Event</div>
+							<div class="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-blue-400 border border-blue-400">In Person</div>
+						</div>
+						<div class="flex flex-col grow">
+							<h4 class="font-semibold text-3xl text-courageous-plum">AIDS is a Civil Rights Issue</h4>
+							<time class="text-courageous-plum-400 font-medium tracking-tighter">Monday, January 15, 2024</time>
+							<p class="mt-4 leading-relaxed">This is a big event.</p>
+						</div>
+						<div class="flex text-sm text-courageous-plum items-center border-t pt-4 border-courageous-plum-200">
+							<div class="font-medium pe-4">AIDS Healthcare Foundation</div>
+							<div class="border-courageous-plum-300 border-s px-4">Southern Region</div>
+							<div class="border-courageous-plum-300 border-s px-4 grow">Atlanta, GA</div>
+							<a href="/event/we-the-people" class="w-fit inline-flex items-center px-4 py-2 text-sm font-medium text-center text-courageous-plum border-2 border-courageous-plum rounded-lg hover:bg-courageous-plum-300 hover:text-white focus:ring-4 focus:outline-none focus:ring-courageous-plum-400 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+								More info
+								<svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+								</svg>
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
